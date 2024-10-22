9c05ada8ee131b039b86e22d756591f11e4e5ea7
Kyle Johnsen <johnsenkyle13@gmail.com>
2024-10-22 14:19:15 -0400

    overhaul model, haven't tuned parameters yet

--- run_sim.py
+++ run_sim.py
@@ -33,112 +33,83 @@ if not results_dir.exists():
 generate_video = False
 
 
-po = 0.1
-alpha = 5
+p0 = 0.1
+unit_len = 50 * b2.um
+sigma = 5 * unit_len
+sigma_strong = 7 * unit_len
 p1 = 1
-alpha1 = 7
 # 14-15 are the time constants for synaptic currents
-tauampa = 2 * b2.ms
-taugaba = 5 * b2.ms
-ex = 2.71828
+tau_ampa = 2 * b2.ms
+tau_gaba = 5 * b2.ms
 # these two determine the number of neurons
-numofexcneur = 10000
-numofinhneur = 2500
-excstartingval = np.random.uniform(-0.5, 0.5, numofexcneur)
-inhstartingval = np.random.uniform(-0.5, 0, numofinhneur)
-excreset = -5 * b2.volt
-inhreset = 0 * b2.volt
-g = 0.1
-# i is the index of a neuron
-# "The strengths of the connections were selected to balance the averaged excitatory current with the averaged inhibitory current in network"
-# 37-38 show the structure of the recurrent currents, however this is not complete and need to be fixed
-# Isynext_equations = '''Isynext = g/n(sum(se*exciteneurons.w)+1/ni*sum(si*inhneurons.w)) : amp'''
-# Isyninh_equations = '''1/n*sum(se*we) : amp'''
-
-# sum() is handled by (summed)
-# ni is handled N_incoming
-# N_outgoing (?)
+N_exc = 10000
+N_inh = 2500
+N_exc_strong = 1000
+N_syn_strong = 500
+g_weak = 0.1
+g_strong = 2
+if realistic_values := True:
+    g_exc = 1 / (100 * b2.Mohm)
+    g_inh = 1 / (200 * b2.Mohm)
+    tau_M = 15 * b2.ms
+    C_exc = tau_M * g_exc
+    C_inh = tau_M * g_inh
+else:
+    g_exc = g_inh = b2.siemens
+    C_exc = C_inh = b2.farad
 
 # %%
 # define the equations and variables for the excitatory and inhibitory neurons
-exceqs = """dv/dt = (I_summed - v/gexc)/C : volt
-C: farad 
-I_summed=Iext+Isynext+Isynweakext+Isyninh+I_default+I_opto: amp
+exceqs = """dv/dt = (I_summed - v*g_exc)/C_exc : volt
+I_summed = I_stim + I_exc_strong + I_exc_weak + I_inh + I_opto : amp
 I_opto : amp
-Iext = stimulus_exc(t,i)*amp: amp
-I_default: amp
-Isynext: amp
-Isyninh: amp
-Isynweakext: amp
-gexc: ohm
+I_stim = stimulus_exc(t,i) * amp : amp
+I_exc_weak : amp
+I_exc_strong : amp
+I_inh : amp
 x = .05*((floor(i/100.0)) - 50)*mmeter : meter
 y = .05*((i%100.0) - 50)*mmeter : meter
 z: meter
 sampled_neurons: boolean
-excthreshold: volt"""
+thresh: volt"""
 
-inheqs = """dv/dt = (Iext + Isynext - v/ginh)/C :volt
-C: farad
-# Iext = stimulus_inh(t,i)*amp : amp
-Iext : amp
-Isynext: amp
-Isyninh : amp
-ginh: ohm
-x: meter
-y: meter
-z: meter
-excthreshold: volt"""
+inheqs = """dv/dt = (I_stim + I_exc - v*g_inh)/C_inh :volt
+# I_stim = stimulus_inh(t,i)*amp : amp
+I_stim : amp
+I_exc: amp
+x = .1*((floor(i/50)) - 25) * mm : meter
+y = .1*((i % 50) - 25) * mm : meter
+z = .5 * mm : meter
+thresh: volt"""
 
-resetv = "v =  excreset"
-excthreshold = "v > excthreshold"
-# NOTE: Iext is currently set to 100 across all neurons, however this is not true for the final model, and needs to be fixed (2ms input)
-exciteneurons = b2.NeuronGroup(
-    numofexcneur, exceqs, threshold=excthreshold, reset=resetv
+
+# NOTE: I_stim is currently set to 100 across all neurons, however this is not true for the final model, and needs to be fixed (2ms input)
+ng_exc = b2.NeuronGroup(
+    N_exc,
+    exceqs,
+    threshold="v > thresh",
+    reset="v = -5*volt",
+    namespace={"g_exc": 1 / (100 * b2.Mohm)},
 )
-inhneurons = b2.NeuronGroup(
-    numofinhneur, inheqs, threshold=excthreshold, reset="v = 0*volt"
-)
+ng_inh = b2.NeuronGroup(N_inh, inheqs, threshold="v > thresh", reset="v = 0*volt")
 
-random_sampled_neurons = rnd.sample(range(0, numofexcneur - 1), 1000)
-sampled_neurons_bool = np.zeros((numofexcneur), dtype=bool)
+random_sampled_neurons = rnd.sample(range(0, N_exc - 1), 1000)
+sampled_neurons_bool = np.zeros((N_exc), dtype=bool)
 sampled_neurons_bool[random_sampled_neurons] = True
-# exciteneurons.x = '2*((floor(i/50.0)) - 50)' #value for 2500 exc neuron
-# exciteneurons.y = '2*((i%50.0) - 50)' #value for 2500 exc neurons
-# exciteneurons.x = '.1*1*((floor(i/10.0)) - 5)*mmeter' #value for 100 exc neuron
-# exciteneurons.y = '.1*1*((i%10.0) - 5)*mmeter' #value for 100 exc neurons
-# exciteneurons.x = '.1*((floor(i/100.0)) - 50)*mmeter' #value for 10000 exc neuron
-# exciteneurons.y = '.1*((i%100.0) - 50)*mmeter' #value for 10000 exc neurons
-exciteneurons.z = np.random.uniform(0.45, 0.55, numofexcneur) * b2.mm
+ng_exc.z = np.random.uniform(0.45, 0.55, N_exc) * b2.mm
 # Initial conditions for neurons
-exciteneurons.v = excstartingval * b2.volt
-exciteneurons.C = np.ones(numofexcneur) * 1 * b2.farad
-exciteneurons.gexc = np.ones(numofexcneur) * (0.1) * b2.ohm
-# exciteneurons.Iext = np.ones(numofexcneur)*100*amp;
-# exciteneurons.Iext = 'stimulus_exc(t,i)*amp'
-# exciteneurons.I_default = np.ones(numofexcneur)*100*amp;
-exciteneurons.excthreshold = np.random.uniform(0.5, 2, numofexcneur) * b2.volt
-exciteneurons.sampled_neurons = sampled_neurons_bool
+ng_exc.v = np.random.uniform(-0.5, 1, N_exc) * b2.volt
+ng_exc.thresh = np.random.uniform(0.5, 2, N_exc) * b2.volt
+ng_exc.sampled_neurons = sampled_neurons_bool
 
-# inhneurons.x = '8*((floor(i/12.5)) - 50)' #value for 625 inh neurons
-# inhneurons.y = '8*((i % 12.5) - 50)' #value for 625 inh neurons
-inhneurons.x = ".1*((floor(i/50)) - 25)*mmeter"
-inhneurons.y = ".1*((i % 50) - 25)*mmeter"
-inhneurons.z = ".5*mmeter"
-inhneurons.v = inhstartingval * b2.volt
-inhneurons.C = np.ones(numofinhneur) * 1 * b2.farad
-inhneurons.ginh = np.ones(numofinhneur) * (0.2) * b2.ohm
-# inhneurons.Iext = np.ones(numofinhneur)*100*amp;
-# stimulus_inh = TimedArray(np.hstack([[0, 0, c, 0, 0]
-#                                 for c in np.random.rand(numofinhneur)]),
-#                                dt=2*ms)
-# inhneurons.Iext = 'stimulus_inh(t)*amp'
-inhneurons.excthreshold = np.random.uniform(0, 1, numofinhneur) * b2.volt
+ng_inh.v = np.random.uniform(-0.5, 0.5, N_inh) * b2.volt
+ng_inh.thresh = np.random.uniform(0, 1, N_inh) * b2.volt
 
 # %%
 # configure initial stimulus
 stim_radius = 0.5 * b2.mm
-stim_arr_exc = np.zeros((2, numofexcneur))  # T x N
-dist_from_center = np.sqrt(exciteneurons.x_**2 + exciteneurons.y_**2) * b2.meter
+stim_arr_exc = np.zeros((2, N_exc))  # T x N
+dist_from_center = np.sqrt(ng_exc.x_**2 + ng_exc.y_**2) * b2.meter
 i2stim = dist_from_center < stim_radius
 print(f"Number of excitatory neurons stimulated: {np.sum(i2stim)}")
 
@@ -149,7 +120,7 @@ print(f"stimulating at {stim_level} amps")
 stimulus_exc = b2.TimedArray(stim_arr_exc, dt=2 * b2.ms)
 
 
-# stim_arr_inh = np.zeros((2, numofinhneur))  # T x N
+# stim_arr_inh = np.zeros((2, N_inh))  # T x N
 # dist_from_center = np.sqrt(inhneurons.x_**2 + inhneurons.y_**2) * b2.meter
 # i2stim = dist_from_center < stim_radius
 # print(f"Number of inhibitory neurons stimulated: {np.sum(i2stim)}")
@@ -161,128 +132,128 @@ stimulus_exc = b2.TimedArray(stim_arr_exc, dt=2 * b2.ms)
 
 # %%
 # synapses
-# 84-88 defines the weak excitatory synapses
-sexcweak = b2.Synapses(
-    exciteneurons,
-    exciteneurons,
-    """w=40000 : 1
-dse/dt = -se/tauampa : amp (clock-driven)
-Isynweakext_post= g/N_incoming*se*w : amp (summed)
-""",
-    on_pre="se = se + .5*amp",
+syn_model = """
+    ds/dt = -s/tau : amp (clock-driven)
+    I_NAME_post= w / N_incoming * s : amp (summed)
+"""
+on_pre = "s += 0.5*amp"
+# original paper used Manhattan distance
+# p="p0*exp(-abs(x_pre-x_post)/(.05*mmeter)*abs(y_pre-y_post) / sigma**2)",
+# Euclidean distance instead:
+connect_prob = "p0 * exp(-((x_pre - x_post)**2 + (y_pre - y_post)**2) / sigma**2)"
+
+syn_e2e_weak = b2.Synapses(
+    ng_exc,
+    ng_exc,
+    syn_model.replace("NAME", "exc_weak"),
+    on_pre=on_pre,
+    namespace={"w": 40000, "tau": tau_ampa},
 )
-sexcweak.connect(
+syn_e2e_weak.connect(
     condition="i!=j",
-    # original paper used Manhattan distance
-    # p="""po*exp((-abs(x_pre-x_post)/(.05*mmeter)*abs(y_pre-y_post)/(.05*mmeter))/((alpha**2)))""",
-    # Euclidean distance instead:
-    p="""po*exp(-sqrt((x_pre - x_post)**2 + (y_pre - y_post)**2) / (.05*mm * alpha**2))""",
+    p=connect_prob,
 )
-# sexcweak.w = .2;
+print(f"Number of weak synapses: {len(syn_e2e_weak)}")
 
+# %%
+# NOTE: why do they hardcode in a 0.1 vs. 2 factor for weak vs strong? do they have the same base weight?
 # 90-96 defines the strong excitatory synapses, note that the total number needs to be capped at 500 to replicate the paper
-sexcstrong = b2.Synapses(
-    exciteneurons,
-    exciteneurons,
-    """w=80000 : 1
-dse/dt = -se/tauampa: amp (clock-driven)
-Isynext_post = w*g/N_incoming*se : amp (summed)
-""",
-    on_pre="se = se + .5*amp",
+i_strong_neurons = np.random.choice(range(N_exc), N_exc_strong, replace=False)
+x_strong = ng_exc.x[i_strong_neurons]
+y_strong = ng_exc.y[i_strong_neurons]
+p_strong_connect = p1 * np.exp(
+    (
+        (10 * unit_len) ** 2
+        - (x_strong - x_strong[:, None]) ** 2
+        - (y_strong - y_strong[:, None]) ** 2
+    )
+    / sigma_strong**2
 )
-
-sexcstrong.connect(
-    condition="i!=j and sampled_neurons_pre and sampled_neurons_post",
-    # p="""p1*exp((100-(abs(x_pre-x_post)/(.05*mmeter)*abs(y_pre-y_post)/(.05*mmeter)))/((alpha1**2)))""",
-    p="""p1*exp((100 - sqrt((x_pre - x_post)**2 + (y_pre - y_post)**2) / (.05*mm)) / alpha1**2)""",
+np.fill_diagonal(p_strong_connect, 0)
+assert p_strong_connect.shape == (N_exc_strong, N_exc_strong)
+strong_connections = np.random.rand(N_exc_strong, N_exc_strong) < p_strong_connect
+i_syn_strong, j_syn_strong = np.where(strong_connections)
+j_syn_strong
+idx_strong_to_keep = np.random.choice(
+    range(len(i_syn_strong)), N_syn_strong, replace=False
 )
+assert len(idx_strong_to_keep) == N_syn_strong
+i_syn_strong = i_syn_strong[idx_strong_to_keep]
+j_syn_strong = j_syn_strong[idx_strong_to_keep]
+assert len(i_syn_strong) == N_syn_strong
+# map back from index among strong neurons to index among all neurons
+i_syn_strong_orig = i_strong_neurons[i_syn_strong]
+j_syn_strong_orig = i_strong_neurons[j_syn_strong]
+assert len(i_syn_strong_orig) == len(j_syn_strong_orig) == N_syn_strong
 
+
+# %%
+syn_e2e_strong = b2.Synapses(
+    ng_exc,
+    ng_exc,
+    syn_model.replace("NAME", "exc_strong"),
+    on_pre=on_pre,
+    namespace={"w": 80000, "tau": tau_ampa},
+)
+syn_e2e_strong.connect(i=i_syn_strong_orig, j=j_syn_strong_orig)
+print(f"Number of strong synapses (should be 500): {len(syn_e2e_strong)}")
 # sexcstrong.w = 1;
 
-# 99-113 defines the inhibitory to excitatory, excitatory to inhibitory, and inhibitory-inhibitory synapses
-inhtoexcsynapse = b2.Synapses(
-    inhneurons,
-    exciteneurons,
-    """w=-10000000 : 1
-dsi/dt = -si/taugaba : amp (clock-driven)
-Isyninh_post = w*g/N_incoming/N_outgoing*si : amp (summed)
-""",
-    on_pre="si = si + .5*amp",
-)
+# %%
 
-inhtoexcsynapse.connect(
+syn_i2e = b2.Synapses(
+    ng_inh,
+    ng_exc,
+    syn_model.replace("NAME", "inh"),
+    on_pre=on_pre,
+    namespace={"w": -10000000, "tau": tau_gaba},
+)
+syn_i2e.connect(
     condition="i!=j",
-    # p="""po*exp((-abs(x_pre-x_post)/(.05*mmeter)*abs(y_pre-y_post)/(.05*mmeter))/((alpha**2)))""",
-    p="""po*exp(-sqrt((x_pre - x_post)**2 + (y_pre - y_post)**2) / (.05*mm * alpha**2))""",
+    p=connect_prob,
 )
 
-exctoinhsynapse = b2.Synapses(
-    exciteneurons,
-    inhneurons,
-    """w=10000000 : 1
-dsi/dt = -si/taugaba : amp (clock-driven)
-Isynext_post = w*g/N_incoming/N_outgoing*si : amp (summed)
-""",
-    on_pre="si = si + .5*amp",
+syn_e2i = b2.Synapses(
+    ng_exc,
+    ng_inh,
+    syn_model.replace("NAME", "exc"),
+    on_pre=on_pre,
+    namespace={"w": 10000000, "tau": tau_ampa},
 )
-
-exctoinhsynapse.connect(
+syn_e2i.connect(
     condition="i!=j",
-    # p="""po*exp((-abs(x_pre-x_post)/(.05*mmeter)*abs(y_pre-y_post)/(.05*mmeter))/((alpha**2)))""",
-    p="""po*exp(-sqrt((x_pre - x_post)**2 + (y_pre - y_post)**2) / (.05*mm * alpha**2))""",
+    p=connect_prob,
 )
 
-# exctoinhsynapse.w = 2; #this needs to be changed
+# exc_st_mon = b2.StateMonitor(ng_exc, ["v", "I_stim", "Isynext"], record=True)
+exc_st_mon = b2.StateMonitor(ng_exc, ["v"], record=True)
+exc_spike_mon = b2.SpikeMonitor(ng_exc)
 
-# a starting point to define the synaptic currents (S7), please not that this is a state variable of excite neurons
-# exciteneurons.Isynext = 'g/numofexcneur*(sum(sexcstrong[:, j].se*sexcstrong[:, j].w)+1/numofinhneur*sum(inhneurons[:, j].si*inhneurons[:, j].w)) '
-
-excitestate = b2.StateMonitor(exciteneurons, ["v", "Iext", "Isynext"], record=True)
-excitespikes = b2.SpikeMonitor(exciteneurons)
-
-# exciteneurons = SpikeGeneratorGroup(numofexcneur, indices, times)
+# exciteneurons = SpikeGeneratorGroup(N_exc, indices, times)
 
 net = b2.Network(
-    exciteneurons,
-    inhneurons,
-    sexcweak,
-    sexcstrong,
-    exctoinhsynapse,
-    inhtoexcsynapse,
-    excitestate,
-    excitespikes,
+    ng_exc,
+    ng_inh,
+    syn_e2e_weak,
+    syn_e2e_strong,
+    syn_e2i,
+    syn_i2e,
+    exc_st_mon,
+    exc_spike_mon,
 )
-
-
-# In[27]:
-
-
-incoming_connections = sexcweak.N_incoming_post
-np.delete(incoming_connections, np.where(incoming_connections == 0))
 
 
 # In[3]:
 simple_opsin = cleo.opto.ProportionalCurrentOpsin(
     name="simple_opsin",
     # handpicked gain to make firing rate roughly comparable to EIF
-    I_per_Irr=-2400000 * b2.amp,
+    I_per_Irr=-2400000 * b2.amp / (b2.mwatt / b2.mm2),
 )
 fiber = cleo.light.Light(
     coords=(1.75, 1.75, 0) * b2.mm,
     light_model=cleo.light.fiber473nm(R0=0.2 * b2.mm),
     name="fiber",
 )
-
-plotargs = {
-    "colors": ["xkcd:fuchsia"],
-    "xlim": (-2.5, 2.5),
-    "ylim": (-2.5, 2.5),
-    "zlim": (0, 1),
-    "scatterargs": {"s": 1},
-}
-scatterargs = {
-    "s": 1,
-}
 
 spikes = cleo.ephys.SortedSpiking(
     name="spikes",
@@ -297,12 +268,13 @@ plotargs = {
     "xlim": (-2.5, 2.5),
     "ylim": (-2.5, 2.5),
     "zlim": (0, 1),
-    "scatterargs": {"s": 1},  # to adjust neuron marker size
+    "scatterargs": {"s": 1},
     "axis_scale_unit": b2.mm,
 }
 
 fig, ax = cleo.viz.plot(
-    exciteneurons,
+    ng_exc,
+    # ng_inh,
     **plotargs,
     devices=[probe, fiber],
 )
@@ -310,9 +282,9 @@ fig.savefig(f"{results_dir}/100x100_static_setup.png")
 
 sim = cleo.CLSimulator(net)
 # sim.inject_stimulator(opto, exciteneurons, Iopto_var_name='I_opto')
-sim.inject(simple_opsin, exciteneurons, Iopto_var_name="I_opto")
-sim.inject(fiber, exciteneurons)
-sim.inject(probe, exciteneurons)
+sim.inject(simple_opsin, ng_exc, Iopto_var_name="I_opto")
+sim.inject(fiber, ng_exc)
+sim.inject(probe, ng_exc)
 
 
 stim_vals = []
@@ -322,10 +294,10 @@ spike_vals = []
 
 class ReactiveLoopOpto(cleo.ioproc.LatencyIOProcessor):
     def __init__(self):
-        super().__init__(sample_period_ms=0.2)
+        super().__init__(sample_period=0.2 * b2.ms)
 
     # since this is open-loop, we don't use state_dict
-    def process(self, state_dict, time_ms):
+    def process(self, state_dict, t_samp):
         i, t, z_t = state_dict["Probe"]["spikes"]
         if np.size(i) >= 3:
             if opto_on:
@@ -335,17 +307,18 @@ class ReactiveLoopOpto(cleo.ioproc.LatencyIOProcessor):
         else:
             opto_intensity = 0
         stim_vals.append(opto_intensity)
-        stim_t.append(time_ms)
+        stim_t.append(t_samp / b2.ms)
         spike_vals.append(np.size(i))
+        opto_intensity *= b2.mwatt / b2.mm2
         # return output dict and time
-        return ({"fiber": opto_intensity}, time_ms + delay_ms)
+        return ({"fiber": opto_intensity}, t_samp + delay_ms * b2.ms)
 
 
 sim.set_io_processor(ReactiveLoopOpto())
 
 if generate_video:
     vv = cleo.viz.VideoVisualizer(dt=0.5 * b2.ms, devices_to_plot=[probe, fiber])
-    sim.inject(vv, exciteneurons)
+    sim.inject(vv, ng_exc)
 
 
 # %%
@@ -366,11 +339,11 @@ print(f"Time elapsed: {t_end - t_start:.1f} seconds")
 
 stim_t_extended = np.copy(stim_t)
 stim_t_extended = np.append(stim_t_extended, np.max(stim_t) + stim_t[1] - stim_t[0])
-spike_counts, bin_edges = np.histogram(excitespikes.t / b2.ms, bins=stim_t_extended)
+spike_counts, bin_edges = np.histogram(exc_spike_mon.t / b2.ms, bins=stim_t_extended)
 
 indexes_half_radius = []
 
-for i in range(numofexcneur):
+for i in range(N_exc):
     if (
         (
             (0.05 * ((np.floor(i / 100.0)) - 50) * b2.mm - 1.75 * b2.mm) ** 2
@@ -383,20 +356,20 @@ for i in range(numofexcneur):
 # %%
 np.savez_compressed(
     results_dir / "data.npz",
-    t_spk_ms=excitespikes.t / b2.ms,
-    i_spk=excitespikes.i,
+    t_spk_ms=exc_spike_mon.t / b2.ms,
+    i_spk=exc_spike_mon.i,
     stim_t=stim_t,
     stim_vals=stim_vals,
-    fiber_t=np.array(fiber.t_ms),
+    fiber_t=np.array(fiber.t / b2.ms),
     fiber_vals=np.array(fiber.values).squeeze(),
     spike_counts=spike_counts,
     bin_edges=bin_edges,
     spike_vals=spike_vals,
     stim_t_extended=stim_t_extended,
     indexes_half_radius=indexes_half_radius,
-    numofexcneur=numofexcneur,
-    exc_x_mm=exciteneurons.x / b2.mm,
-    exc_y_mm=exciteneurons.y / b2.mm,
+    numofexcneur=N_exc,
+    exc_x_mm=ng_exc.x / b2.mm,
+    exc_y_mm=ng_exc.y / b2.mm,
 )
 
 # %%
@@ -406,7 +379,7 @@ np.savez_compressed(
 # plot_all(
 #     excitestate,
 #     excitespikes,
-#     numofexcneur,
+#     N_exc,
 #     stim_t,
 #     stim_vals,
 #     stim_t_extended,
